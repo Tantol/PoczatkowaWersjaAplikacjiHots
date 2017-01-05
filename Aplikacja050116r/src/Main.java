c57cbd31c944c3da26ca3ae73f66f8925d9bb89e
Tantol <tomando5218@gmail.com>
2017-01-05 23:36:20 +0100

    added all characters [names]

--- Aplikacja050116r/src/Main.java
+++ Aplikacja050116r/src/Main.java
@@ -3,11 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		String[] names = { "Abathur","Alarak" ,"Anub'arak" ,"Artanis" ,"Arthas" ,"Auriel" ,"Azmodan" ,"Brightwing","Chen","Chromie","Dehaka","Diablo","E.T.C.","Falstad","Gazlowe","Greymane","Gul'dan","Illidan","Jaina","Johanna","Kael'thas","Kerrigan","Kharazim","Leoric","Li Li","Li-Ming","Lt.Morales","Lunara","Malfurion","Medivh","Muradin","Murky","Nazeebo","Nova","Ragnaros","Raynor","Rehgar","Rexxar","Samuro","Sgt.Hammer","Sonya","Stitches","Sylvanas","Tassadar","The Butcher","The Lost Vikings","Thrall","Tracer","Tychus","Tyrael","Tyrande","Uther","Valla","Varian","Xul","Zagara","Zarya","Zeratul","Zul'jin"};
-		int co5nazwa=0;
+		int after5=0;
 		for(String name : names){
-			co5nazwa++;
+			after5++;
 			System.out.print(name+" | ");
-			if(co5nazwa%5==0){System.out.print("\n");}
+			if(after5%5==0){System.out.print("\n");}
 
 	}
 
