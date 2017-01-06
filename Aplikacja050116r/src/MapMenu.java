c6d95705ee7a4490b08963fcd4c2dcebd84337aa
DoneQ <marcinrubach@gmail.com>
2017-01-06 18:30:59 +0100

    Debugged Buttnos

--- Aplikacja050116r/src/MapMenu.java
+++ Aplikacja050116r/src/MapMenu.java
@@ -1,7 +1,9 @@
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
-public class MapMenu extends JFrame {
+public class MapMenu extends JFrame implements ActionListener{
 private JButton map;
 String[] names = {"Haunted Mines","Towers of Doom","Infernal Shrines","Battlefield of Eternity","Tomb of The Spider Quoeen","Sky Temple","Garden of Terror","Blackheart's Bay","Dragon Shire","Cursed Hollow","Braxis Holdout","Warhead Junction"};
 	
@@ -15,10 +17,21 @@ String[] names = {"Haunted Mines","Towers of Doom","Infernal Shrines","Battlefie
 		for(String name: names){
 		map = new JButton(name);
 		add(map);
+		map.addActionListener(this);
 		}
-		
 		
 		setVisible(true);
 	}
-
+	
+	public void actionPerformed(ActionEvent event){
+		String label = event.getActionCommand();
+		for(String name: names){
+			if(label.equals(name)){
+				System.out.println(name);
+			}		
+		
+		}
+		
+		}
+	
 }
