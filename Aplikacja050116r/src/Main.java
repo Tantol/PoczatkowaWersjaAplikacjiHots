65ca3bf34e0a04a266ba98ab8db46061a9e35c71
Tantol <tomando5218@gmail.com>
2017-01-06 18:10:20 +0100

    Add main menu, not end

--- Aplikacja050116r/src/Main.java
+++ Aplikacja050116r/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			 public void run() {
-				 new CharacterMenu();
+				 new MainMenu();
 				 }
 			 });
 		}
---
+++ Aplikacja050116r/src/MainMenu.java
@@ -0,0 +1,76 @@
+	import javax.swing.*;
+	import java.awt.event.ActionEvent;
+	import java.awt.event.ActionListener;
+	import java.awt.FlowLayout;
+
+	public class MainMenu extends JFrame implements ActionListener{
+		//private JLabel informationAboutChampion;
+		private JButton option;
+		String[] names = { "Map", "Champion" };
+		
+		public MainMenu(){
+			super("Choose your option");
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setSize(800,400);
+			setLocationRelativeTo(null);
+			setLayout(new FlowLayout(FlowLayout.CENTER, 10, 50));
+			
+			for(String name : names){
+				option = new JButton(name);
+				add(option);
+				option.addActionListener(this);
+			}
+			
+			
+			JMenuBar menubar = new JMenuBar();
+			JMenu menuFile = new JMenu("File");
+			JMenuItem menuClose = new JMenuItem("Close");
+			JMenuItem menuAboutTheProgram = new JMenu("About the program");
+			menuFile.add(menuAboutTheProgram);
+			menuFile.add(new JSeparator());
+			menuFile.add(menuClose);
+			menubar.add(menuFile);
+			setJMenuBar(menubar);
+			
+			setVisible(true);
+			
+		}
+		
+		public void actionPerformed(ActionEvent e){
+			String label = e.getActionCommand();
+			
+			if(label.equals("Map")){}
+			
+		}
+
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 String label = e.getActionCommand();
+ if(label.equals("Otworz plik"))
+
+	Object zrodlo = e.getSource();
+ if(zrodlo == oblicz)
+ {
+ try {
+ double a = Double.parseDouble(element1.getText());
+ double b = Double.parseDouble(element2.getText());
+
+ wynik.setText(a*b+"");
+ }
+ catch (NumberFormatException ev) {
+ JOptionPane.showMessageDialog(null, "Wpisz poprawne
+ wartosci!", "Blad argumentow!?",
+JOptionPane.ERROR_MESSAGE);
+ }
+ }
+*/
+	 
+	
