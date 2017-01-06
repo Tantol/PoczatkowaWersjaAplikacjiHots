93e2ac2a4125ee21d09ac5656c6e6fc39219f864
Tantol <tomando5218@gmail.com>
2017-01-06 20:51:36 +0100

    W ten sposob?

--- Aplikacja050116r/src/Characters/Abathur.java
+++ Aplikacja050116r/src/Characters/Abathur.java
@@ -2,7 +2,13 @@ package Characters;
 
 
 public class Abathur extends MotherOfAllChampions {
-	
+	static String name = "Abathur";
+	static int universe = 1;
+	static int role = 3;
+	public Abathur(){
+		super(name, universe, role);
+	}
 
+	
 	
 }
--- Aplikacja050116r/src/Characters/MotherOfAllChampions.java
+++ Aplikacja050116r/src/Characters/MotherOfAllChampions.java
@@ -1,8 +1,19 @@
 package Characters;
 
 
-public class MotherOfAllChampions {
-	String[] role = { "Warrior", "Assasin", "Support", "Specialist"};
-	String[] universe = { "Warcraft", "StarCraft", "Diablo", "Retro", "Overwatch" };
+public abstract class MotherOfAllChampions {
+	 private String[] tabRole = { "Warrior", "Assasin", "Support", "Specialist"};
+	 private String[] tabUniverse = { "Warcraft", "StarCraft", "Diablo", "Retro", "Overwatch" };
+	 private String role;
+	 private String universe;
+	 private String name;
+	 
+	 public MotherOfAllChampions(String name, int universe, int role) {
+		 this.role=tabRole[role];
+		 this.universe=tabUniverse[universe];
+		 this.name=name;
+
+	   }
+
 
 }
