8bf6504347fcb8a06ae55e14b83daa12388b3d46
Tantol <tomando5218@gmail.com>
2017-01-06 23:35:07 +0100

    MotherOfAllChampions-ChampionList;

--- Aplikacja050116r/src/Characters/ChampionList.java
+++ Aplikacja050116r/src/Characters/ChampionList.java
@@ -1,19 +1,14 @@
 package Characters;
 
 
-public abstract class MotherOfAllChampions {
+public class ChampionList {
 	 private String[] tabRole = { "Warrior", "Assasin", "Support", "Specialist"};
 	 private String[] tabUniverse = { "Warcraft", "StarCraft", "Diablo", "Retro", "Overwatch" };
 	 private String role;
 	 private String universe;
 	 private String name;
 	 
-	 public MotherOfAllChampions(String name, int universe, int role) {
-		 this.role=tabRole[role];
-		 this.universe=tabUniverse[universe];
-		 this.name=name;
 
-	   }
 
 
 }
