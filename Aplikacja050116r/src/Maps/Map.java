5ceb9d8b89ddc0fcf299173faad521f5e2e6ba6b
DoneQ <marcinrubach@gmail.com>
2017-01-06 23:00:51 +0100

    Maps Again

--- Aplikacja050116r/src/Maps/Map.java
+++ Aplikacja050116r/src/Maps/Map.java
@@ -3,9 +3,31 @@ package Maps;
 public class Map {
 	String name;
 	int lanes;
-	
-	
+	int forts;
+	int keeps;
+	int keepHP;
+	int towers;
+	int towerHP;
+	int gates;
+	int gateHP;
+	int fountains;
+	int fountainHP;
+	int coreHP;
+	int coreShieldHP;
 	public Map(){
+		name = "NoName";
+		lanes = 0;
+		forts = 0;
+		keeps = 0;
+		keepHP = 0;
+		towers = 0;
+		towerHP = 0;
+		gates = 0;
+		gateHP = 0;
+		fountains = 0;
+		fountainHP = 0;
+		coreHP = 0;
+		coreShieldHP = 0;
 		
 	}
 }
