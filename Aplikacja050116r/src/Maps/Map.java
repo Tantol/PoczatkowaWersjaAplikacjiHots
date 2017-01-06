066548cc2670c89021e444496768b6ad15356f99
DoneQ <marcinrubach@gmail.com>
2017-01-06 23:48:08 +0100

    Git Commit Push ^^

--- Aplikacja050116r/src/Maps/Map.java
+++ Aplikacja050116r/src/Maps/Map.java
@@ -1,5 +1,7 @@
 package Maps;
 
+import java.awt.List;
+
 public class Map {
 	String name;
 	int lanes;
@@ -30,4 +32,33 @@ public class Map {
 		coreShieldHP = 0;
 		
 	}
+	
+	public void setName(String n){name = n;}
+	public String getName(){return name;}
+	public void setLanes(int l){lanes = l;}
+	public int getLanes(){return lanes;}
+	public void setForts(int f){forts = f;}
+	public int getForts(){return forts;}
+	public void setKeeps(int k){keeps = k;}
+	public int getKeeps(){return keeps;} 
+	public void setKeepHP(int hp){keepHP=hp;}
+	public int getKeepHP(){return keepHP;}
+	public void setTowers(int t){towers = t;}
+	public int getTowers(){return towers;}
+	public void setTowerHP(int hp){towerHP = hp;}
+	public int getTowersHP(){return towerHP;}
+	public void setGates(int g){gates = g;}
+	public int getGates(){return gates;}
+	public void setGateHP(int hp){gateHP = hp;}
+	public int getGateHP(){return gateHP;}
+	public void setFountains(int f){fountains = f;}
+	public int getFountains(){return fountains;}
+	public void setFountainHP(int hp){fountainHP = hp;}
+	public int getFountainsHP(){return fountainHP;}
+	public void setCoreHP(int hp){coreHP = hp;}
+	public int getCoreHP(){return coreHP;}
+	public void setCoreShieldHP(int hp){coreShieldHP = hp;}
+	public int getCoreShieldHP(){return coreShieldHP;}
+	
+	
 }
---
+++ Aplikacja050116r/src/Maps/MapList.java
@@ -0,0 +1,189 @@
+package Maps;
+
+public class MapList {
+	public MapList(){
+	// Haunted Mines
+	Map hauntedMines = new Map();
+	hauntedMines.setName("Haunted Mines");
+	hauntedMines.setLanes(0);
+	hauntedMines.setCoreHP(0);
+	hauntedMines.setCoreShieldHP(0);
+	hauntedMines.setForts(0);
+	hauntedMines.setFountainHP(0);
+	hauntedMines.setFountains(0);
+	hauntedMines.setGateHP(0);
+	hauntedMines.setGates(0);
+	hauntedMines.setKeepHP(0);
+	hauntedMines.setKeeps(0);
+	hauntedMines.setTowerHP(0);
+	hauntedMines.setTowers(0);
+	// Towers of Doom
+	Map towersOfDoom = new Map();
+	towersOfDoom.setName("Towers Of Dooms");
+	towersOfDoom.setLanes(0);
+	towersOfDoom.setCoreHP(0);
+	towersOfDoom.setCoreShieldHP(0);
+	towersOfDoom.setForts(0);
+	towersOfDoom.setFountainHP(0);
+	towersOfDoom.setFountains(0);
+	towersOfDoom.setGateHP(0);
+	towersOfDoom.setGates(0);
+	towersOfDoom.setKeepHP(0);
+	towersOfDoom.setKeeps(0);
+	towersOfDoom.setTowerHP(0);
+	towersOfDoom.setTowers(0);
+	// Infernal Shrines
+	Map infernalShrines = new Map();
+	infernalShrines.setName("Infernal Shrines");
+	infernalShrines.setLanes(0);
+	infernalShrines.setCoreHP(0);
+	infernalShrines.setCoreShieldHP(0);
+	infernalShrines.setForts(0);
+	infernalShrines.setFountainHP(0);
+	infernalShrines.setFountains(0);
+	infernalShrines.setGateHP(0);
+	infernalShrines.setGates(0);
+	infernalShrines.setKeepHP(0);
+	infernalShrines.setKeeps(0);
+	infernalShrines.setTowerHP(0);
+	infernalShrines.setTowers(0);
+	// Battlefield Of Eternity
+	Map battlefieldOfEternity = new Map();
+	battlefieldOfEternity.setName("Battlefield Of Eternity");
+	battlefieldOfEternity.setLanes(0);
+	battlefieldOfEternity.setCoreHP(0);
+	battlefieldOfEternity.setCoreShieldHP(0);
+	battlefieldOfEternity.setForts(0);
+	battlefieldOfEternity.setFountainHP(0);
+	battlefieldOfEternity.setFountains(0);
+	battlefieldOfEternity.setGateHP(0);
+	battlefieldOfEternity.setGates(0);
+	battlefieldOfEternity.setKeepHP(0);
+	battlefieldOfEternity.setKeeps(0);
+	battlefieldOfEternity.setTowerHP(0);
+	battlefieldOfEternity.setTowers(0);
+	// Tomb Of The Spider Queen
+	Map tombOfTheSpiderQueen = new Map();
+	tombOfTheSpiderQueen.setName("Tomb Of The Spider Queen");
+	tombOfTheSpiderQueen.setLanes(0);
+	tombOfTheSpiderQueen.setCoreHP(0);
+	tombOfTheSpiderQueen.setCoreShieldHP(0);
+	tombOfTheSpiderQueen.setForts(0);
+	tombOfTheSpiderQueen.setFountainHP(0);
+	tombOfTheSpiderQueen.setFountains(0);
+	tombOfTheSpiderQueen.setGateHP(0);
+	tombOfTheSpiderQueen.setGates(0);
+	tombOfTheSpiderQueen.setKeepHP(0);
+	tombOfTheSpiderQueen.setKeeps(0);
+	tombOfTheSpiderQueen.setTowerHP(0);
+	tombOfTheSpiderQueen.setTowers(0);
+	// Sky Temple
+	Map skyTemple = new Map();
+	skyTemple.setName("Sky Temple");
+	skyTemple.setLanes(0);
+	skyTemple.setCoreHP(0);
+	skyTemple.setCoreShieldHP(0);
+	skyTemple.setForts(0);
+	skyTemple.setFountainHP(0);
+	skyTemple.setFountains(0);
+	skyTemple.setGateHP(0);
+	skyTemple.setGates(0);
+	skyTemple.setKeepHP(0);
+	skyTemple.setKeeps(0);
+	skyTemple.setTowerHP(0);
+	skyTemple.setTowers(0);
+	// Gareden of Terror
+	Map gardenOfTerror = new Map();
+	gardenOfTerror.setName("Gareden of Terror");
+	gardenOfTerror.setLanes(0);
+	gardenOfTerror.setCoreHP(0);
+	gardenOfTerror.setCoreShieldHP(0);
+	gardenOfTerror.setForts(0);
+	gardenOfTerror.setFountainHP(0);
+	gardenOfTerror.setFountains(0);
+	gardenOfTerror.setGateHP(0);
+	gardenOfTerror.setGates(0);
+	gardenOfTerror.setKeepHP(0);
+	gardenOfTerror.setKeeps(0);
+	gardenOfTerror.setTowerHP(0);
+	gardenOfTerror.setTowers(0);
+	// Blackheart's Bay
+	Map blackheartsBay = new Map();
+	blackheartsBay.setName("Blackheart's Bay");
+	blackheartsBay.setLanes(0);
+	blackheartsBay.setCoreHP(0);
+	blackheartsBay.setCoreShieldHP(0);
+	blackheartsBay.setForts(0);
+	blackheartsBay.setFountainHP(0);
+	blackheartsBay.setFountains(0);
+	blackheartsBay.setGateHP(0);
+	blackheartsBay.setGates(0);
+	blackheartsBay.setKeepHP(0);
+	blackheartsBay.setKeeps(0);
+	blackheartsBay.setTowerHP(0);
+	blackheartsBay.setTowers(0);
+	// Dragon Shire
+	Map dragonShire = new Map();
+	dragonShire.setName("Dragon Shire");
+	dragonShire.setLanes(0);
+	dragonShire.setCoreHP(0);
+	dragonShire.setCoreShieldHP(0);
+	dragonShire.setForts(0);
+	dragonShire.setFountainHP(0);
+	dragonShire.setFountains(0);
+	dragonShire.setGateHP(0);
+	dragonShire.setGates(0);
+	dragonShire.setKeepHP(0);
+	dragonShire.setKeeps(0);
+	dragonShire.setTowerHP(0);
+	dragonShire.setTowers(0);
+	// Cursed Hollow
+	Map cursedHollow = new Map();
+	cursedHollow.setName("Cursed Hollow");
+	cursedHollow.setLanes(0);
+	cursedHollow.setCoreHP(0);
+	cursedHollow.setCoreShieldHP(0);
+	cursedHollow.setForts(0);
+	cursedHollow.setFountainHP(0);
+	cursedHollow.setFountains(0);
+	cursedHollow.setGateHP(0);
+	cursedHollow.setGates(0);
+	cursedHollow.setKeepHP(0);
+	cursedHollow.setKeeps(0);
+	cursedHollow.setTowerHP(0);
+	cursedHollow.setTowers(0);
+	// Braxis Holdout
+	Map braxisHoldout = new Map();
+	braxisHoldout.setName("Braxis Holdout");
+	braxisHoldout.setLanes(0);
+	braxisHoldout.setCoreHP(0);
+	braxisHoldout.setCoreShieldHP(0);
+	braxisHoldout.setForts(0);
+	braxisHoldout.setFountainHP(0);
+	braxisHoldout.setFountains(0);
+	braxisHoldout.setGateHP(0);
+	braxisHoldout.setGates(0);
+	braxisHoldout.setKeepHP(0);
+	braxisHoldout.setKeeps(0);
+	braxisHoldout.setTowerHP(0);
+	braxisHoldout.setTowers(0);
+	// Warhead Junction
+	Map warheadJunction = new Map();
+	warheadJunction.setName("Warhead Junction");
+	warheadJunction.setLanes(0);
+	warheadJunction.setCoreHP(0);
+	warheadJunction.setCoreShieldHP(0);
+	warheadJunction.setForts(0);
+	warheadJunction.setFountainHP(0);
+	warheadJunction.setFountains(0);
+	warheadJunction.setGateHP(0);
+	warheadJunction.setGates(0);
+	warheadJunction.setKeepHP(0);
+	warheadJunction.setKeeps(0);
+	warheadJunction.setTowerHP(0);
+	warheadJunction.setTowers(0);
+
+		
+	
+	}
+}
