60c48c3a3f755ff307885522158a27a401118aa1
Tantol <tomando5218@gmail.com>
2017-01-06 23:52:05 +0100

    added some fuction to champion

--- Aplikacja050116r/src/Characters/Champion.java
+++ Aplikacja050116r/src/Characters/Champion.java
@@ -6,9 +6,18 @@ public class Champion {
 	public Abilitie heroSkill1;
 	public Abilitie heroSkill2;
 	public String name;
+	public String role; // "Warrior", "Assasin", "Support", "Specialist"
+	public String universe; // "Warcraft", "StarCraft", "Diablo", "Retro", "Overwatch"
+	public String info;
+	public double defaultHp;
+	
 	
 	public Champion(String name){
 		this.name=name;
+		this.role="";
+		this.universe="";
+		this.info="";
+		this.defaultHp=0;
 	}
 	
 	public void skillQ(String name){this.skillQ(name);}
@@ -17,7 +26,19 @@ public class Champion {
 	public void passiveD(String name){this.passiveD(name);}
 	public void heroSkill1(String name){this.heroSkill1(name);}	
 	public void heroSkill2(String name){heroSkill2(name);}
-		
 	
+	//Set
+	public void name(String name){this.name=name;}
+	public void role(String role){this.role=role;}
+	public void universe(String universe){this.universe=universe;}
+	public void info(String info){this.info=info;}
+	public void defaultHp(double defaultHp){this.defaultHp=defaultHp;}
+	
+	//Get
+	public String name(){return name;}
+	public String role(){return role;}
+	public String universe(){return universe;}
+	public String info(){return info;}
+	public double defaultHp(){return defaultHp;}
 
 }
--- Aplikacja050116r/src/Characters/ChampionList.java
+++ Aplikacja050116r/src/Characters/ChampionList.java
@@ -2,11 +2,7 @@ package Characters;
 
 
 public class ChampionList {
-	 private String[] tabRole = { "Warrior", "Assasin", "Support", "Specialist"};
-	 private String[] tabUniverse = { "Warcraft", "StarCraft", "Diablo", "Retro", "Overwatch" };
-	 private String role;
-	 private String universe;
-	 private String name;
+	
 	 
 
 
